577eaf00f893eddb4a1876b8dd45e4be0648682d
mdruchek <124770059+mdruchek@users.noreply.github.com>
2024-07-01 07:49:34 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,9 +4,9 @@ from datetime import datetime, timedelta
 import pytest
 sys.path.append('../test_github_ci_python_advanced')
 
-from ../app import create_app
-from ../app.database import db as _db
-from ../app.models import Client, Parking, ClientParking
+from ..app import create_app
+from ..app.database import db as _db
+from ..app.models import Client, Parking, ClientParking
 
 
 @pytest.fixture
